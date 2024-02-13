a5b2b67ca3d1257cead709484d1b4eb3ad3da82d
mwg396 <michael.graham1029@gmail.com>
2024-02-12 20:20:56 -0600

    comment

--- models/blogpost.js
+++ models/blogpost.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Blogpost extends Model {}
-
+//depolying
 Blogpost.init(
   {
     id: {
